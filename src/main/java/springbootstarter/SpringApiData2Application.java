0bb05e8b3084fa47ca508c3e762777dc95224171
huyifan <huyifan@umich.edu>
2020-04-06 16:47:16 -0400

    Initial commit

---
+++ src/main/java/springbootstarter/SpringApiData2Application.java
@@ -0,0 +1,13 @@
+package springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiData2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiData2Application.class, args);
+	}
+
+}
---
+++ src/main/java/springbootstarter/topic/TopicService.java
@@ -0,0 +1,41 @@
+package springbootstarter.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+	@Autowired
+	private TopicRepository topicRepository;
+	
+	public List<Topic> getAllTopics() {
+		List<Topic> topics = new ArrayList<>();
+		topicRepository.findAll().forEach(topics::add);
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+//		return topics.stream().filter(t ->t.getId().equals(id)).findFirst().get();
+		return topicRepository.findById(id).orElse(null);
+	}
+
+	public void addTopic(Topic topic) {
+		topicRepository.save(topic);
+		
+	}
+
+	public void updateTopic(Topic topic, String id) {
+		topicRepository.save(topic);
+		
+	}
+
+	public void deleteTopic(String id) {
+		topicRepository.deleteById(id);
+		
+	}
+}
